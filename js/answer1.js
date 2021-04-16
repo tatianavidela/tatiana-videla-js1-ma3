42ea4e9f40fe6e3a06e9baff1af23519a67eabfe
tatianavidela <tatianavidelaliz@gmail.com>
2021-04-16 14:28:19 +0200

    attempt 1

---
+++ js/answer1.js
@@ -0,0 +1,13 @@
+// Question 1
+// Convert the function below to an arrow function:
+// function division(a, b) {    
+//     return a % b;
+// }
+
+const division =  (a,b) => {
+    return a % b; 
+}
+
+const result = division(10,5)
+
+console.log (result)
---
+++ js/answer2.js
@@ -0,0 +1,57 @@
+// Question 2
+// Make a call to the following API endpoint:
+// https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating
+
+// Loop through the results and display the following properties in HTML, but only for the first eight results:
+// name
+// rating
+// number of tags (not the tag details, just the amount of tags)
+// The styling for this assignment is not important but loading indicator should be displayed while the API call is in progress.
+
+// Be sure to handle any potential errors in the code.
+
+// You can use either regular promise or async/await syntax to make the call.
+
+// Be sure to arrange all file types appropriately, consult the repos from the lessons for examples.
+
+
+const url =  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating" ;
+
+const resultsContainer = document.querySelector(".results")
+
+
+async function makeApiCall (){
+
+    try{
+        const response = await fetch(url);
+        const results = await response.json();
+    
+        const items= results.results;
+    
+    
+        console.log(results.results)
+    
+        resultsContainer.innerHTML = ""
+    
+        for  (let i = 0; i < items.length; i++ ) {
+    
+            if (i === 8) {
+                break
+            }
+    
+            resultsContainer.innerHTML += 
+            `<div class="result"> 
+            <p>Name: ${items[i].name}</p> 
+            <p>Rating: ${items[i].rating}</p> 
+            <p>Amount of tags: ${items[i].tags.length} </p> 
+            </div>`;
+        }
+    } 
+    catch (error) {
+        resultsContainer.innerHTML = "Sorry. An error occurred"
+    }
+
+
+}
+
+makeApiCall();

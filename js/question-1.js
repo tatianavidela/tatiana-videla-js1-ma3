c6d4a1447827dab0b105ab71bf4d8996b040dc3d
tatianavidela <tatianavidelaliz@gmail.com>
2021-04-17 11:52:44 +0200

    updated file names

--- js/question-1.js
+++ js/question-1.js
@@ -1,8 +1,4 @@
 // Question 1
-// Convert the function below to an arrow function:
-// function division(a, b) {    
-//     return a % b;
-// }
 
 const division =  (a,b) => {
     return a % b; 
--- js/question-2.js
+++ js/question-2.js
@@ -1,19 +1,4 @@
-// Question 2
-// Make a call to the following API endpoint:
-// https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating
-
-// Loop through the results and display the following properties in HTML, but only for the first eight results:
-// name
-// rating
-// number of tags (not the tag details, just the amount of tags)
-// The styling for this assignment is not important but loading indicator should be displayed while the API call is in progress.
-
-// Be sure to handle any potential errors in the code.
-
-// You can use either regular promise or async/await syntax to make the call.
-
-// Be sure to arrange all file types appropriately, consult the repos from the lessons for examples.
-
+//Question 2
 
 const url =  "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating" ;
 
